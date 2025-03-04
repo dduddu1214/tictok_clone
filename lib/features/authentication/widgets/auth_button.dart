e93866c1b0bf2041e9010e0a927d406187d56a86
dduddu1214 <tmfrl1747@naver.com>
2025-03-04 21:24:51 +0900

    auth_button 수정

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/email.screen.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
@@ -8,28 +9,37 @@ class AuthButton extends StatelessWidget {
 
   const AuthButton({super.key, required this.text, required this.icon});
 
+  void emailTap(BuildContext context) {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => EmailScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: Container(
-        padding: EdgeInsets.all(Sizes.size14),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey.shade300, width: Sizes.size1),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Align(alignment: Alignment.centerLeft, child: icon),
-            Text(
-              text,
-              style: TextStyle(
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: () => emailTap(context),
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Container(
+          padding: EdgeInsets.all(Sizes.size14),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade300, width: Sizes.size1),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Align(alignment: Alignment.centerLeft, child: icon),
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
